09802563f0178ca87b1da1052ae1f10be7062d9a
matvlaale <maculaali@yandex.ru>
2020-07-27 14:25:21 +0300

    Lesson_1

--- app/src/main/java/ru/startandroid/android_komsomol/MainActivity.java
+++ app/src/main/java/ru/startandroid/android_komsomol/MainActivity.java
@@ -2,7 +2,13 @@ package ru.startandroid.android_komsomol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.method.KeyListener;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button mainButton = findViewById(R.id.mainButton);
+        mainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, WeatherCardActivity.class));
+            }
+        });
     }
 }
