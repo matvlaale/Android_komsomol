4ec8151cb30d5b644cc34cb0a13f66b3ac573b4c
matvlaale <maculaali@yandex.ru>
2020-08-12 23:46:42 +0300

    Lesson_6

---
+++ app/src/main/java/ru/startandroid/android_komsomol/RecyclerDataAdapter.java
@@ -0,0 +1,92 @@
+package ru.startandroid.android_komsomol;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecyclerDataAdapter extends RecyclerView.Adapter<RecyclerDataAdapter.ViewHolder> {
+    private ArrayList<String> data;
+    private IRVOnItemClick onItemClickCallback;
+
+    public RecyclerDataAdapter(ArrayList<String> data, IRVOnItemClick onItemClickCallback) {
+        this.data = data;
+        this.onItemClickCallback = onItemClickCallback;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_rv_layout, parent,
+                false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String text = data.get(position);
+
+        holder.setTextToTextView(text);
+        holder.setOnClickForItem(text);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data == null ? 0 : data.size();
+    }
+
+    void add(String newElement) {
+        data.add(newElement);
+        notifyItemInserted(data.size() - 1);
+    }
+
+    void remove() {
+        if(data.size() > 0) {
+            data.remove(0);
+            notifyItemRemoved(0);
+        }
+    }
+
+    void move() {
+        if(data.size() > 2) {
+            String item = data.get(2);
+            data.remove(2);
+            data.add(0, item);
+            notifyItemMoved(2, 0);
+        }
+    }
+
+    void clearList() {
+        data = new ArrayList<>();
+        notifyDataSetChanged();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView textView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.itemTextView);
+        }
+
+        void setTextToTextView(String text) {
+            textView.setText(text);
+        }
+
+        void setOnClickForItem(final String text) {
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(onItemClickCallback != null) {
+                        onItemClickCallback.onItemClicked(text);
+                    }
+                }
+            });
+        }
+    }
+}
